9ee5e71fc87371bc31e21e6bca4d2a261a1a1e62
fernando0315 <fernando_nxt@hotmail.com>
2017-02-16 02:31:28 -0800

    Test data & small debug

--- CCircuit.cpp
+++ CCircuit.cpp
@@ -20,7 +20,7 @@ void node::calcOut()
 	}
 	else if (gateType == Gate::XOR)
 		for(int i = 1; i<input.size(); i++)
-			output |= *input[i];
+			output ^= *input[i];
 	else if(gateType == Gate::NAND)
 	{
 		for (int i = 1; i < input.size(); i++)
@@ -43,6 +43,7 @@ void node::calcOut()
 
 void CCircuit::addGate(Gate gType, string gateName, vector<string> inputPtr)
 {
+	static int numOfGateDefined = 1;
 	node *newNode = new node(gType);
 
 	for (string i : inputPtr)
@@ -54,8 +55,9 @@ void CCircuit::addGate(Gate gType, string gateName, vector<string> inputPtr)
 				cout << "Circuit is not well defined" << endl;
 				exit(1);
 			}
-
+			cout << "Added to gate: " << i << endl;
 			node *nodePtr = gatesPtr[i];
+			gatesPtr[i]->lastOutput = false;
 			newNode->input.push_back(&(nodePtr->output));
 		}
 		else
@@ -66,7 +68,7 @@ void CCircuit::addGate(Gate gType, string gateName, vector<string> inputPtr)
 		}
 	}
 	cktOutput[gateName] = &(newNode->output);
-	gatesPtr[gateName] = newNode;
+	gatesPtr[to_string(numOfGateDefined++)] = newNode;
 }
 
 void CCircuit::printTruthTable()
@@ -74,17 +76,25 @@ void CCircuit::printTruthTable()
 	int inputSize = cktInput.size();
 	int maxValue = (int)pow(2, inputSize);
 
+	for (map<string, bool>::iterator it = cktInput.begin(); it != cktInput.end(); it++)
+		cout << it->first << '\t';
+	cout << '|' << '\t';
+	for (map<string, bool*>::iterator it = cktOutput.begin(); it != cktOutput.end(); it++)
+		cout << it->first << '\t';
+	cout << endl;
+	cout << "--------------------------------|-----------------------------\n";
+
 	for (int i = 0; i < maxValue; i++)
 	{
 		bitset<8> inputValue(i);
-		int j = 0;
+		int j = inputSize - 1;
 		for (map<string, bool>::iterator it = cktInput.begin(); it != cktInput.end(); it++)
-			it->second = inputValue[j++];
+			it->second = inputValue[j--];
 		
 		for (unordered_map<string, node*>::iterator it = gatesPtr.begin(); it != gatesPtr.end(); it++)
 			it->second->calcOut();
 	
-		for (map<string, bool>::reverse_iterator it = cktInput.rbegin(); it != cktInput.rend(); it++)
+		for (map<string, bool>::iterator it = cktInput.begin(); it != cktInput.end(); it++)
 			cout << it->second << '\t';
 
 		cout << "|\t";
--- main.cpp
+++ main.cpp
@@ -2,13 +2,44 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include "CCircuit.h"
 
 using namespace std;
 
 int main()
 {
-	vector<bool*> t(4);
-	cout << "halo" << endl;
+	CCircuit c1;
+	vector<string> inputList1;
+	inputList1.push_back("A");
+	inputList1.push_back("B");
+	c1.addGate(XNOR, "XNOR1", inputList1);
 
+	vector<string> inputList2;
+	inputList2.push_back("1");
+	inputList2.push_back("C");
+	c1.addGate(XNOR, "SUM", inputList2);
 
+	vector<string> inputList3;
+	inputList3.push_back("A");
+	inputList3.push_back("C");
+	c1.addGate(AND, "AND2", inputList3);
+
+	vector<string> inputList4;
+	inputList4.push_back("A");
+	inputList4.push_back("B");
+	c1.addGate(AND, "AND1", inputList4);
+
+	vector<string> inputList5;
+	inputList5.push_back("B");
+	inputList5.push_back("C");
+	c1.addGate(AND, "AND3", inputList5);
+
+	vector<string> inputList6;
+	inputList6.push_back("3");
+	inputList6.push_back("4");
+	inputList6.push_back("5");
+	c1.addGate(OR, "CARRY", inputList6);
+
+	c1.printTruthTable();
 }
+
